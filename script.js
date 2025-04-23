241e3457bfa82d246c1497db0756754deb2e5ac4
Kennard6969 <kennardwijaya9@gmail.com>
2025-04-23 12:11:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function tampil() {
+	document.getElementById("Alert").style.display = "block";
+	document.getElementById("overlay").style.display = "flex";
+}
+
+function tutup() {
+	document.getElementById("Alert").style.display = "none";
+	document.getElementById("overlay").style.display = "none";
+}
